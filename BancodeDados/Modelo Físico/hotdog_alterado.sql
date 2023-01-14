c7a31c3243203e92f152740c2c5a82ae41ac7399
Marina <maarinagomes1@gmail.com>
2023-01-14 15:28:45 -0300

    Create hotdog_alterado.sql

---
+++ BancodeDados/Modelo Físico/hotdog_alterado.sql
@@ -0,0 +1,334 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 29-Nov-2022 às 03:18
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `hotdog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idCliente` int(10) NOT NULL,
+  `cpfCliente` varchar(14) NOT NULL,
+  `nomeCliente` varchar(100) NOT NULL,
+  `telefoneCliente` varchar(15) NOT NULL,
+  `emailCliente` varchar(50) NOT NULL,
+  `senhaCliente` varchar(20) NOT NULL,
+  `tipoLogradouro` varchar(100) NOT NULL,
+  `logradouro` varchar(100) NOT NULL,
+  `numero` varchar(5) NOT NULL,
+  `complemento` varchar(100) DEFAULT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `municipio` varchar(100) NOT NULL,
+  `cep` varchar(10) DEFAULT NULL,
+  `estado` char(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`idCliente`, `cpfCliente`, `nomeCliente`, `telefoneCliente`, `emailCliente`, `senhaCliente`, `tipoLogradouro`, `logradouro`, `numero`, `complemento`, `bairro`, `municipio`, `cep`, `estado`) VALUES
+(1, '123.456.789-10', 'Fulano', '(21)91234-5678', 'fulano@gmail.com', 'hotdog123', 'rua', 'General Baiano', '934', 'ap101', 'Miguel Couto', 'Nova Iguaçu', '26-157.000', 'RJ'),
+(2, '123.456.789-11', 'Ciclano', '(21)91234-6789', 'ciclano@gmail.com', 'pizza123', 'Avenida', 'Itapemerim', '123', 'fundos', 'Luiz de Lemos', 'Nova Iguaçu', '16-153.000', 'RJ');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `idDepartamento` int(10) NOT NULL,
+  `nomeDepartamento` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `departamento`
+--
+
+INSERT INTO `departamento` (`idDepartamento`, `nomeDepartamento`) VALUES
+(1, 'vendas');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `fornecedor`
+--
+
+CREATE TABLE `fornecedor` (
+  `idFornecedor` int(10) NOT NULL,
+  `cnpj` varchar(18) NOT NULL,
+  `nomeFornecedor` varchar(100) NOT NULL,
+  `telefoneFornecedor` varchar(15) NOT NULL,
+  `nomeGerente` varchar(100) NOT NULL,
+  `tipoLogradouro` varchar(100) NOT NULL,
+  `logradouro` varchar(100) NOT NULL,
+  `numero` varchar(5) NOT NULL,
+  `complemento` varchar(100) DEFAULT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `municipio` varchar(100) NOT NULL,
+  `estado` char(2) NOT NULL,
+  `cep` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `fornecedor`
+--
+
+INSERT INTO `fornecedor` (`idFornecedor`, `cnpj`, `nomeFornecedor`, `telefoneFornecedor`, `nomeGerente`, `tipoLogradouro`, `logradouro`, `numero`, `complemento`, `bairro`, `municipio`, `estado`, `cep`) VALUES
+(2, '12.345.678/0001-91', 'Latícinios Milano', '(21)91234-9876', 'Danilo', 'rua', 'Dr.Cunha', '45', 'ap101', 'centro', 'Nova Iguaçu', 'RJ', '12-159.00');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `idFuncionario` int(10) NOT NULL,
+  `cpfFuncionario` varchar(14) NOT NULL,
+  `nomeFuncionario` varchar(100) NOT NULL,
+  `telefoneFuncionario` varchar(15) NOT NULL,
+  `emailFuncionario` varchar(50) NOT NULL,
+  `senhaFuncionario` varchar(20) DEFAULT NULL,
+  `tipoLogradouro` varchar(100) NOT NULL,
+  `logradouro` varchar(100) NOT NULL,
+  `numero` varchar(5) NOT NULL,
+  `complemento` varchar(100) DEFAULT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `municipio` varchar(100) NOT NULL,
+  `estado` char(2) NOT NULL,
+  `cep` varchar(9) NOT NULL,
+  `idDepartamento` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `funcionario`
+--
+
+INSERT INTO `funcionario` (`idFuncionario`, `cpfFuncionario`, `nomeFuncionario`, `telefoneFuncionario`, `emailFuncionario`, `senhaFuncionario`, `tipoLogradouro`, `logradouro`, `numero`, `complemento`, `bairro`, `municipio`, `estado`, `cep`, `idDepartamento`) VALUES
+(1, '987.654.321.98', 'Gabriel', '(21)99876-5432', 'gabriel@gmail.com', 'gbzada123', 'rua', 'livia guida', '974', 'ap101', 'Miguel Couto', 'Nova Iguaçu', '26', 'RJ', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `itenspedido`
+--
+
+CREATE TABLE `itenspedido` (
+  `idItensPedido` int(10) NOT NULL,
+  `idPedido` int(10) NOT NULL,
+  `idProduto` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `itenspedido`
+--
+
+INSERT INTO `itenspedido` (`idItensPedido`, `idPedido`, `idProduto`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `idPedido` int(10) NOT NULL,
+  `dataPedido` datetime NOT NULL,
+  `idCliente` int(10) NOT NULL,
+  `idFuncionario` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `pedido`
+--
+
+INSERT INTO `pedido` (`idPedido`, `dataPedido`, `idCliente`, `idFuncionario`) VALUES
+(1, '2022-11-28 13:42:34', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `idProduto` int(10) NOT NULL,
+  `nomeProduto` varchar(100) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `preco` decimal(10,0) NOT NULL,
+  `idFornecedor` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`idProduto`, `nomeProduto`, `categoria`, `descricao`, `preco`, `idFornecedor`) VALUES
+(1, 'HotDog de cafta', 'principal', 'Carne, cebolas carameladas, bacon', '20', 2),
+(2, 'Pepsi', 'bebida', 'Pepsi 2L', '10', 2);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idCliente`),
+  ADD UNIQUE KEY `cpfCliente` (`cpfCliente`),
+  ADD UNIQUE KEY `emailCliente` (`emailCliente`),
+  ADD UNIQUE KEY `senhaCliente` (`senhaCliente`);
+
+--
+-- Índices para tabela `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`idDepartamento`);
+
+--
+-- Índices para tabela `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  ADD PRIMARY KEY (`idFornecedor`),
+  ADD UNIQUE KEY `cnpj` (`cnpj`);
+
+--
+-- Índices para tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`idFuncionario`),
+  ADD UNIQUE KEY `cpfFuncionario` (`cpfFuncionario`),
+  ADD UNIQUE KEY `emailFuncionario` (`emailFuncionario`),
+  ADD UNIQUE KEY `senhaFuncionario` (`senhaFuncionario`),
+  ADD KEY `idDepartamento` (`idDepartamento`);
+
+--
+-- Índices para tabela `itenspedido`
+--
+ALTER TABLE `itenspedido`
+  ADD PRIMARY KEY (`idItensPedido`),
+  ADD KEY `idPedido` (`idPedido`),
+  ADD KEY `idProduto` (`idProduto`);
+
+--
+-- Índices para tabela `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`idPedido`),
+  ADD KEY `idCliente` (`idCliente`),
+  ADD KEY `idFuncionario` (`idFuncionario`);
+
+--
+-- Índices para tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`idProduto`),
+  ADD KEY `idFornecedor` (`idFornecedor`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idCliente` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `departamento`
+--
+ALTER TABLE `departamento`
+  MODIFY `idDepartamento` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  MODIFY `idFornecedor` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `idFuncionario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `itenspedido`
+--
+ALTER TABLE `itenspedido`
+  MODIFY `idItensPedido` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `idPedido` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `idProduto` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD CONSTRAINT `funcionario_ibfk_1` FOREIGN KEY (`idDepartamento`) REFERENCES `departamento` (`idDepartamento`) ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `itenspedido`
+--
+ALTER TABLE `itenspedido`
+  ADD CONSTRAINT `itenspedido_ibfk_1` FOREIGN KEY (`idPedido`) REFERENCES `pedido` (`idPedido`),
+  ADD CONSTRAINT `itenspedido_ibfk_2` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`idProduto`) ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`),
+  ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`idFuncionario`) REFERENCES `funcionario` (`idFuncionario`) ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`idFornecedor`) REFERENCES `fornecedor` (`idFornecedor`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
