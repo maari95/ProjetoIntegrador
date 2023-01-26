6233ff98176e2cb7f5eab633ada8efc543f31db9
Joao-Pedro <joao3001int@gmail.com>
2023-01-26 13:32:11 -0300

    Create conexaobanco.php

---
+++ Projeto Cachorro Quente/conexaobanco.php
@@ -0,0 +1,21 @@
+<?php
+
+class db{
+	private $host='localhost';
+	private $usuario='root';
+	private $senha='';
+	
+	private $database='dogprime';
+	
+    public function conecta_mysql(){
+	$con=mysqli_connect($this->host,$this->usuario,$this->senha,$this->database);
+	
+	mysqli_set_charset($con,'utf8');
+	
+	if(mysqli_connect_errno()){
+		echo 'Erro ao tentar conexão com o BD.'.mysqli_connect_error();
+}
+	return $con;
+}
+}
+?>
