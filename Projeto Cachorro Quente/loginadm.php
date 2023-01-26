9e721a09c07de93a395b3c0d337ddaafa2a64d77
Joao-Pedro <joao3001int@gmail.com>
2023-01-26 15:53:25 -0300

    Create loginadm.php

---
+++ Projeto Cachorro Quente/loginadm.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once("conexaobanco.php");
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$objDb = new db();
+
+$link = $objDb -> conecta_mysql();
+
+$verifica = ("SELECT * FROM funcionario WHERE emailFuncionario = '$email' AND senhaFuncionario = '$senha'");
+$resultado = mysqli_query($link, $verifica);
+
+if(mysqli_num_rows($resultado) > 0)
+{
+    $_SESSION['emailFuncionario'] = $email;
+    $_SESSION['senhaFuncionario'] = $senha;
+    header('location:index.html');
+}else{
+    unset($_SESSION['emailFuncionario']);
+    unset($_SESSION['senhaFuncionario']);
+    header('location:admin.html');
+}
+
