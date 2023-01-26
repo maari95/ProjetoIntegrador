2d6e85f08602d398060a1ea031e612aaa0668b43
Joao-Pedro <joao3001int@gmail.com>
2023-01-26 14:06:46 -0300

    Update login.php

--- Projeto Cachorro Quente/login.php
+++ Projeto Cachorro Quente/login.php
@@ -1,20 +1,20 @@
 <?php
 session_start();
 require_once("conexaobanco.php");
-$Email = $_POST['email'];
+$email = $_POST['email'];
 $senha = $_POST['senha'];
 $objDb = new db();
 
 $link = $objDb -> conecta_mysql();
 
-$verifica = ("SELECT * FROM cliente WHERE emailCliente ='$email' AND senhaCliente = '$senha'") or die("erro ao selecionar");
-$resultado = mysqli_query($link, $resultado);
-echo $resultado;
+$verifica = ("SELECT * FROM cliente WHERE emailCliente ='$email' AND senhaCliente = '$senha'");
+$resultado = mysqli_query($link, $verifica);
+
 if(mysqli_num_rows($resultado) > 0)
 {
     $_SESSION['emailCliente'] = $email;
     $_SESSION['senhaCliente'] = $senha;
-    header('location:index.html');
+    header('location:home.html');
 }else{
     unset($_SESSION['emailCliente']);
     unset($_SESSION['senhaCliente']);
