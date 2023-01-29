15b5cfe06cf753260684856f1dc707b6c34fb037
Gabriel Bernardo <gabrielb.lopess44@gmail.com>
2023-01-28 23:34:56 -0300

    Update main.js

--- Projeto Cachorro Quente/main.js
+++ Projeto Cachorro Quente/main.js
@@ -28,4 +28,16 @@ const handlePhone = (event) => {
   }
 
 
+  const inputcep = document.getElementById("campo");
 
+  input.addEventListener("keyup", formatarCep);
+  
+  function formatarCep(e){
+  
+  var v= e.target.value.replace(/\D/g,"")                
+  
+  v=v.replace(/^(\d{5})(\d)/,"$1-$2") 
+  
+  e.target.value = v;
+  
+  }
