f1898d78558a1a92843e412455c5f8f9a66bf5c4
Gabriel Bernardo <gabrielb.lopess44@gmail.com>
2023-01-30 14:39:18 -0300

    Update mascaras.js

--- Projeto Cachorro Quente/mascaras.js
+++ Projeto Cachorro Quente/mascaras.js
@@ -58,6 +58,24 @@ function formatarTelefone(e) {
 }
 
 //mascara cnpj//
+const input3 = document.getElementById("#cnpj");
+input.addEventListener("keyup", formatarCNPJ);
+function formatarCNPJ(e){
+
+var v= e.target.value.replace(/\D/g,"");
+
+v=v.replace(/^(\d{2})(\d)/,"$1.$2");
+
+v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3");
+
+v=v.replace(/\.(\d{3})(\d)/,".$1/$2");
+
+v=v.replace(/(\d{4})(\d)/,"$1-$2");  
+
+e.target.value = v;
+
+
+}
 
 
 //moeda
